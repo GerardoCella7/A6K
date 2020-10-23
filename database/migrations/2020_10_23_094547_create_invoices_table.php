3009d7f5604bc8a28c201b42c07266163152367a
Albert Ndizeye <albertndizeye@gmail.com>
2020-10-23 11:52:03 +0200

    table creation

---
+++ database/migrations/2020_10_23_094547_create_invoices_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInvoicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime("date_emit");
+            $table->boolean("paid_status")->default(0);
+            $table->integer('bookings_id')->unsigned();
+            $table->foreign('bookings_id')
+                    ->references('id')->on('bookings')
+                    ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoices');
+    }
+}
---
+++ database/migrations/2020_10_23_094945_create_picture_rooms_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePictureRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('picture_rooms', function (Blueprint $table) {
+            $table->id();
+            $table->string("url");
+            $table->integer('rooms_id')->unsigned();
+            $table->foreign('rooms_id')
+                    ->references('id')->on('rooms')
+                    ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('picture_rooms');
+    }
+}
