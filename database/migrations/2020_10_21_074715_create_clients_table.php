55ab9acd7232303ed9b57373cfb8d74ca20084c4
Gerardo Cella <becode@gerardo-cella.net>
2020-10-23 09:43:50 +0200

    Add Models et migration for Client and Material

---
+++ database/migrations/2020_10_21_074715_create_clients_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->text('lastname', 50);
+            $table->text('firstname', 50);
+            $table->text('email');
+            $table->text('adress');
+            $table->text('phone', 25);
+            $table->text('tva', 25);
+            $table->text('company_type', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
