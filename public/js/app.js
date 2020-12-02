58b99be52038663f7f58fd2fe68c01b191e5d9c2
Gerardo Cella <gerardocella7@gmail.com>
2020-12-02 14:07:01 +0100

    merge

--- public/js/app.js
+++ public/js/app.js
@@ -42649,8 +42649,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Hp\BeCode\github\A6K-V2\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Hp\BeCode\github\A6K-V2\resources\css\app.css */"./resources/css/app.css");
+__webpack_require__(/*! D:\BeCode\A6K\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\BeCode\A6K\resources\css\app.css */"./resources/css/app.css");
 
 
 /***/ })
