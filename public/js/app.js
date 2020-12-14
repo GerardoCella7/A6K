addec4d1d95c9059a509fd3c456ecaee968150a9
jcruz97 <cruzjurgen97@gmail.com>
2020-12-14 15:49:05 +0100

    Pull test server.php

--- public/js/app.js
+++ public/js/app.js
@@ -41735,8 +41735,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Hp\BeCode\github\a6k\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Hp\BeCode\github\a6k\resources\css\app.css */"./resources/css/app.css");
+__webpack_require__(/*! C:\Coding\A6K\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Coding\A6K\resources\css\app.css */"./resources/css/app.css");
 
 
 /***/ })
